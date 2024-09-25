3c8ce84052ecfde80803c9e7aaef4d9b57fd753b
Vultus Limunaria <94930859+catsrieldreemurr@users.noreply.github.com>
2024-09-25 17:57:49 +0200

    Added Info Section, Added some more QOL stuff
    Title;

---
+++ BadPasswordGen/main.js
@@ -0,0 +1,40 @@
+ // elements 
+const generate1 = document.getElementById("generate1");
+const field = document.getElementById("field");
+const inputBox = document.getElementById("length");
+
+// other variables 
+const possibleCharacters = "qwertyuiopasdfghjklzxcvbnmQWERTYUIOPADFGHJKLZXCVBNM1234567890@£$€&/%/?*_-&";
+
+// functions
+function check(num){ // check if the number is within boundaries
+    let userLength;
+
+    if(num == 0){
+        userLength = 20;
+    } else if(num < 3){
+        userLength = 3;
+        console.log("b");
+    } else if(num > 100){
+        userLength = 100; 
+    } else{
+        userLength = num;
+    }
+
+    return userLength;
+}
+
+// main code
+generate1.onclick = function(){
+    let string = "";
+    let userInput  = inputBox.value
+    console.log(userInput);
+    let passLength = check(inputBox.value);
+
+    for(let i = 0; i < passLength; i++){
+        let randomNumber = Math.floor(Math.random() * possibleCharacters.length);
+        string = string + possibleCharacters[randomNumber];
+    }
+
+    field.innerHTML = string;
+}
